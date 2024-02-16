892151b6b40cb0bd6fff9050ab050c8d4e0509eb
Akinlolu Ogunleye <akinloludavid2727@gmail.com>
2024-02-16 01:44:21 +0100

    fixed cors issues

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import { CONFIG } from './config/projectConfig'
 import healthRoute from './routes/health'
 import authRoute from './routes/auth'
@@ -12,6 +13,7 @@ const PORT = CONFIG.SERVER.PORT
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 app.use(morgan('dev'))
 app.use('/v1', healthRoute)
 app.use('/v1', authRoute)
